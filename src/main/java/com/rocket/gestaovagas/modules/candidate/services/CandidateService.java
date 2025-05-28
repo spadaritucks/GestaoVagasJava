83cd383deaf153043f6cfcb4b0e9f518fe632e78
spadaritucks <thiago.spadari02@gmail.com>
2025-05-28 10:01:37 -0300

    Adiciona codificação de senha no CandidateService utilizando PasswordEncoder, garantindo a segurança das senhas dos candidatos ao serem salvas no repositório.

--- src/main/java/com/rocket/gestaovagas/modules/candidate/services/CandidateService.java
+++ src/main/java/com/rocket/gestaovagas/modules/candidate/services/CandidateService.java
@@ -1,6 +1,7 @@
 package com.rocket.gestaovagas.modules.candidate.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rocket.gestaovagas.exceptions.UserFoundException;
@@ -13,12 +14,18 @@ public class CandidateService {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
 
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+                var password = passwordEncoder.encode(candidateEntity.getPassword());
+                candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }

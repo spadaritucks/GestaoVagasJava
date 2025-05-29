e4932322d71d12e54116d6015809cae6e9b93007
spadaritucks <thiago.spadari02@gmail.com>
2025-05-29 12:41:29 -0300

    Implementa controle de acesso para candidatos, adicionando a anotação @PreAuthorize no endpoint de obtenção de perfil no CandidateController. Atualiza o AuthCandidateService para utilizar a string "CANDIDATE" ao definir roles no token JWT. Modifica o SecurityCandidateFilter para garantir a autenticação adequada e ajusta a configuração de segurança para habilitar a segurança em métodos. Além disso, remove a linha que limpava o contexto de autenticação no SecurityFilter.

--- src/main/java/com/rocket/gestaovagas/security/SecurityCandidateFilter.java
+++ src/main/java/com/rocket/gestaovagas/security/SecurityCandidateFilter.java
@@ -2,6 +2,8 @@ package com.rocket.gestaovagas.security;
 
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,31 +16,39 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityCandidateFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JWTCandidateProvider jwtProvider;
+  @Autowired
+  private JWTCandidateProvider jwtProvider;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-        SecurityContextHolder.getContext().setAuthentication(null);
-        String header = request.getHeader("Authorization");
+    String header = request.getHeader("Authorization");
 
-        if (request.getRequestURI().startsWith("/candidate")) {
-            if (header != null) {
-              var token = this.jwtProvider.validateToken(header);
-              if (token == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-              }
-              request.setAttribute("candidate_id", token.getSubject());
-              var roles = token.getClaim("roles");
-
+    if (request.getRequestURI().startsWith("/candidate")) {
+      if (header != null) {
+        var token = this.jwtProvider.validateToken(header);
+        if (token == null) {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
         }
+        request.setAttribute("candidate_id", token.getSubject());
+        var roles = token.getClaim("roles").asList(Object.class);
 
-        filterChain.doFilter(request, response);
+        var grants = roles.stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
+            .toList();
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(), null,
+            grants);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+      }
+
+      filterChain.doFilter(request, response);
 
     }
 
-}
+  }
 }
--- src/main/java/com/rocket/gestaovagas/security/SecurityConfig.java
+++ src/main/java/com/rocket/gestaovagas/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.rocket.gestaovagas.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final SecurityCandidateFilter securityCandidateFilter;

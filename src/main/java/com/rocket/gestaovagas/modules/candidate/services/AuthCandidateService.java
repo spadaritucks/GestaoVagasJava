01cb74dc947620f0069aa23afb21b3e1a6ae7444
spadaritucks <thiago.spadari02@gmail.com>
2025-05-29 11:29:19 -0300

    Atualiza AuthCandidateResponseDTO para incluir o campo expires_in e modifica AuthCandidateService para utilizar este novo campo na geração do token JWT. Adiciona permissão para o endpoint /candidate/auth na configuração de segurança.

--- src/main/java/com/rocket/gestaovagas/modules/candidate/services/AuthCandidateService.java
+++ src/main/java/com/rocket/gestaovagas/modules/candidate/services/AuthCandidateService.java
@@ -42,15 +42,18 @@ public class AuthCandidateService {
         }
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
         var token = JWT.create()
                 .withIssuer("javagas")
                 .withSubject(candidate.getId().toString())
                 .withClaim("roles", Arrays.asList("candidate"))
-                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+                .withExpiresAt(expiresIn)
                 .sign(algorithm);
 
         var AuthCandidateResponse = AuthCandidateResponseDTO.builder()
-                .access_token(token).build();
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
 
         return AuthCandidateResponse;
 

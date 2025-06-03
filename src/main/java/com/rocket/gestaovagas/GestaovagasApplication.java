f7ce1fd68fc333de1efdf6e05e1178b64b4583aa
spadaritucks <thiago.spadari02@gmail.com>
2025-06-02 23:52:22 -0300

    Remove anotações OpenAPI da classe GestaovagasApplication e cria nova classe SwaggerConfig para configuração da documentação da API, incluindo informações sobre segurança e descrição da API.

--- src/main/java/com/rocket/gestaovagas/GestaovagasApplication.java
+++ src/main/java/com/rocket/gestaovagas/GestaovagasApplication.java
@@ -3,20 +3,9 @@ package com.rocket.gestaovagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 
-@OpenAPIDefinition(info = @Info(
-	title = "Gestão de Vagas",
-	version = "1",
-	description = "API para gestão de vagas de emprego"
-))
 @SpringBootApplication
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GestaovagasApplication {
 
 	public static void main(String[] args) {
